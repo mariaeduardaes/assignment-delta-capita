3c2ad408ecdb74284e07e62cb7548d9f79e5eb45
maria.santo <maria.santo@cwi.com.br>
2025-08-22 18:38:03 +0200

    test: add setup jest and frontend unit testing

---
+++ src/__tests__/UploadPage.test.tsx
@@ -0,0 +1,61 @@
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import UploadPage from "@/app/page";
+
+// Mock for pdfService
+jest.mock("@/services/pdf", () => ({
+  pdfService: {
+    upload: jest.fn().mockResolvedValue("ok"),
+  },
+}));
+
+describe("UploadPage", () => {
+  it("should render the titles correctly", () => {
+    render(<UploadPage />);
+    expect(screen.getByText("Sign your PDF here")).toBeInTheDocument();
+    expect(
+      screen.getByText("Upload a PDF document for signing it")
+    ).toBeInTheDocument();
+  });
+
+  it("should allow to select a file and show the name", async () => {
+    render(<UploadPage />);
+    const input = screen.getByLabelText("upload") as HTMLInputElement;
+
+    const file = new File(["dummy content"], "example.pdf", {
+      type: "application/pdf",
+    });
+
+    fireEvent.change(input, { target: { files: [file] } });
+
+    // Nome do arquivo deve aparecer
+    expect(await screen.findByText("example.pdf")).toBeInTheDocument();
+  });
+
+  it("should show the message Processing... during the upload", async () => {
+    render(<UploadPage />);
+    const input = screen.getByLabelText("upload") as HTMLInputElement;
+
+    const file = new File(["dummy content"], "example.pdf", {
+        type: "application/pdf",
+    });
+
+    fireEvent.change(input, { target: { files: [file] } });
+
+    // Processing deve aparecer
+    expect(await screen.findByText(/Processing/i)).toBeInTheDocument();
+  });
+
+  it("should clean the input and the state after the upload", async () => {
+    render(<UploadPage />);
+    const input = screen.getByLabelText("upload") as HTMLInputElement;
+
+    const file = new File(["dummy content"], "example.pdf", {
+      type: "application/pdf",
+    });
+
+    fireEvent.change(input, { target: { files: [file] } });
+
+    await waitFor(() => expect(input.value).toBe(""));
+  });
+});
